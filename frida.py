5f2e3ff62f0c83fb7b723bba463f757e63748c0d
Skirlez <skirlez3@gmail.com>
2025-12-19 00:57:51 +0200

    Update frida.py

--- frida.py
+++ frida.py
@@ -133,7 +133,9 @@ def build_gamemaker_project(force = False):
 	elif os.name == "nt":
 		IGOR_OS_SUBFOLDER="windows"
 		IGOR_OUTPUT_PATH="data.win"
-		IGOR_TARGETS=["Windows" "PackageZip"]
+		IGOR_TARGETS=["Windows", "PackageZip"]
+		IGOR_ASSETS_FOLDER=""
+		IGOR_ASSETS_FILTER=["options.ini", "igor.output.manifest", f"{PROJECT_NAME}.exe"]
 
 	IGOR_PATH=f"{GAMEMAKER_CACHE_PATH}/runtimes/runtime-{RUNTIME_VERSION}/bin/igor/{IGOR_OS_SUBFOLDER}/x64/Igor"
 	RUNTIME_PATH=f"{GAMEMAKER_CACHE_PATH}/runtimes/runtime-{RUNTIME_VERSION}"
@@ -182,9 +184,7 @@ def build_gamemaker_project(force = False):
 		print(e)
 		exit()
 
-
-
-	#cleanup()
+	cleanup()
 
 	if (SHOULD_HASH != "0"):
 		new_hash = hash_project()
